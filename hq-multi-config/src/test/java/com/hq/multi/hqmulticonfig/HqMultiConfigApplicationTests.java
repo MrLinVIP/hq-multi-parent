361c67c57e990df4e7e1fb715cfeeb69db7112ee
dreamingwing <1158103993@qq.com>
2019-11-22 09:25:50 +0800

    第三次提交

---
+++ hq-multi-config/src/test/java/com/hq/multi/hqmulticonfig/HqMultiConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hq.multi.hqmulticonfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HqMultiConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
